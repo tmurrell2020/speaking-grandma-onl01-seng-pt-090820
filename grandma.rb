add87529c155187e40b17d2d961ae0936485013a
Terrell Murrell <littlerell12@gmail.com>
2020-09-11 17:24:30 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,8 +1,8 @@
 # Write a speak_to_grandma method.
-def speak_to_grandma(phrase)
-  if phrase == phrase.upcase
+def speak_to_grandma(saying)
+  if saying == saying.upcase
     puts "NO, NOT SINCE 1938!"
-  elsif phrase == "I LOVE YOU GRANDMA!"
+  elsif saying == "I LOVE YOU GRANDMA!"
     puts "I LOVE YOU TOO PUMPKIN!"
   else
     puts "HUH?! SPEAK UP, SONNY!"
